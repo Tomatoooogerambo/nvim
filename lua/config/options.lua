a1e4b5e90e35fabe75d71dba171ba9b95b5d701b
tomatoooo <736170457@qq.com>
2025-02-17 17:37:38 +0800

    latex

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,12 @@
 -- Add any additional options here
 
 vim.opt.winbar = "%=%m %f"
+
+-- Ensure vimtex is configured correctly
+vim.g.vimtex_compiler_method = "latexmk"
+vim.g.vimtex_view_zathura_use_synctex = 0
+vim.g.vimtex_quickfix_mode = 0
+vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_compiler_latexmk_engines = {
+  _ = "-xelatex", -- 或者使用 '-lualatex'
+}
